6872831f7d23a3623687a53384c59d7428607033
leyla li <leylali@qq.com>
2019-12-06 12:54:02 -0800

    add variables

--- class-1.py
+++ class-1.py
@@ -1,4 +1,6 @@
-print("There once was a man named John, ")
-print("he was 70 years old. ")
-print("He really liked the name John, ")
-print("but didn't like being 70.")
+character_name = "Tom"
+character_age = "5"
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
