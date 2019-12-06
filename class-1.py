429fccba5018b4d5f59826cee0f5a662b9a79243
leyla li <leylali@qq.com>
2019-12-06 12:55:38 -0800

    store a number

--- class-1.py
+++ class-1.py
@@ -1,6 +1,8 @@
 character_name = "Tom"
-character_age = "5"
+character_age = 5
 print("There once was a man named " + character_name + ", ")
 print("he was " + character_age + " years old. ")
+
+character_name = "Mikey"
 print("He really liked the name " + character_name + ", ")
 print("but didn't like being " + character_age + ".")
