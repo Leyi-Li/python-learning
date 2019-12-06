9b08f58269d67b801c707454786d14291babab25
leyla li <leylali@qq.com>
2019-12-06 13:01:26 -0800

    backslash usage

--- class-1.py
+++ class-1.py
@@ -1,10 +1,9 @@
 character_name = "Tom"
-character_age = 5
+character_age = "5"
 is_male = True
-print("There once was a man named " + character_name + ", ")
-print("he was " + character_age + " years old. ")
+print("There once was a man named \n" + character_name + ", ")
+print("he\'s " + character_age + " years old. ")
 
 character_name = "Mikey"
 print("He really liked the name " + character_name + ", ")
 print("but didn't like being " + character_age + ".")
-print("Is he a man? The answer is: " + is_male)
