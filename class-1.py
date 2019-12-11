ddcb6c3a6b350bc10ce180218ce45990d8c697ac
leyla li <leylali@qq.com>
2019-12-11 14:06:59 -0800

    built in math functions

--- class-1.py
+++ class-1.py
@@ -24,5 +24,13 @@ print(phrase.replace("there", "Leyla"))
 print(3 + 4.5)
 print(3 * (4 + 5))
 print(10 % 3)
-my_num = 5
+my_num = -5
 print(str(my_num) + " is a good number.")
+
+print(abs(my_num))
+print(pow(3, 2))
+print(max(10, 9))
+print(round(3.2))
+print(round(3.8))
+
+print(floor(3.7))
