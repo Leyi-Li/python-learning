8fe2aa0ffe269e073794c26e668e590c6f7ad9d2
leyla li <leylali@qq.com>
2019-12-11 14:42:42 -0800

    alter elements in a list

--- lists.py
+++ lists.py
@@ -1,5 +1,11 @@
 
-friends = ["Leyla", "Ian", "John", "Colin"]
+friends = ["Leyla", "Ian", "John", "Colin", "Jack", "Dan"]
 
 print(friends)
 print(friends[0])
+print(friends[-1])
+print(friends[1:])
+print(friends[0:3])
+
+friends[4] = "Mike"
+print(friends)
