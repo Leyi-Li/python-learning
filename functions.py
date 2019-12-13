91f9a391796bae9765518f0474b02ba7273e73cd
leyla li <leylali@qq.com>
2019-12-12 16:31:01 -0800

    add a number parameter and convert it to string

--- functions.py
+++ functions.py
@@ -1,13 +1,14 @@
 # indention means its in the function
-def say_hi(name):
-    print("Hello " + name + "!")
+def say_hi(name, age):
+    print("Hello " + name + ", you are " + str(age))
 
 
 # have to call the function
 
 print("top")
-say_hi('Leyla')
+say_hi('Leyla', 25)
 print("bottom")
+
 
 
 
