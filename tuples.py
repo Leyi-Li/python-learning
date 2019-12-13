1e7d4926394a55d1bb686255007384a433a67d24
leyla li <leylali@qq.com>
2019-12-12 16:21:41 -0800

    tuple and created one

---
+++ tuples.py
@@ -0,0 +1,7 @@
+# it's a container to store data, immutable which is not changable, but list can be edible
+coordinates = (4, 5)
+# coordinates[1] = 9 will show an error
+print(coordinates[0])
+
+lists_of_coordinates = [(1, 2), (3, 4), (5, 4)]
+
