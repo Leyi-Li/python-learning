a47c8156c3f39656b65c6b10c3eada9218eddf6b
leyla li <leylali@qq.com>
2019-12-12 16:06:13 -0800

    add or remove functions of lists

--- lists.py
+++ lists.py
@@ -9,3 +9,27 @@ print(friends[0:3])
 
 friends[4] = "Mike"
 print(friends)
+
+# lists functions
+lucky_numbers = [23, 32, 6, 13, 8, 49, 52]
+
+friends.extend(lucky_numbers)
+print(friends)
+# add to the end of the list
+friends.append("Anna")
+print(friends)
+# insert to a certain position
+friends.insert(1, "BB")
+print(friends)
+# remove element
+friends.remove("BB")
+print(friends)
+
+# remove all the elements
+friends.clear()
+print(friends)
+
+# remove the last element
+friends.pop()
+print(friends)
+
