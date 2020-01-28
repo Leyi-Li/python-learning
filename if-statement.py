7a1ecbf6ab875811652a8529777c09e81fd3c9ea
leyla li <leylali@qq.com>
2020-01-28 15:42:13 -0800

    comparison signs

--- if-statement.py
+++ if-statement.py
@@ -29,3 +29,14 @@ elif not(is_male) and is_tall:
     print("You are not a male but tall")
 else:
     print("You are not a male nor tall")
+
+
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+print(max_num(2,3,4))
