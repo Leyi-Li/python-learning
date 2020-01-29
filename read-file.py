777d0792f3cbd92443137d5769ac531d3658ffb7
leyla li <leylali@qq.com>
2020-01-29 15:30:43 -0800

    append, rewrite and create a file

--- read-file.py
+++ read-file.py
@@ -1,6 +1,6 @@
 # r for read, w for write, a for append infor to the end of the file, r+ for read and write
 
-employee_file = open("employess.txt", "r")
+# employee_file = open("employess.txt", "r")
 
 # print(employee_file.readable())
 # print(employee_file.read())
@@ -11,8 +11,32 @@ employee_file = open("employess.txt", "r")
 
 # print(employee_file.readlines())
 
-for employee in employee_file.readlines():
-    print(employee)
+# for employee in employee_file.readlines():
+#     print(employee)
 
 
-employee_file.close()
+##############################         append new info              ####################################
+
+# employee_file = open("employess.txt", "a")
+
+# employee_file.write("Toby - HR")
+#employee_file.write("\nKelly - Customer Service")
+
+
+####################################       rewrite the whole file                     ###################################
+
+employee_file = open("employess.txt", "w")
+
+employee_file.write("Toby - HR")
+
+
+####################################       create a new file                     ###################################
+
+employee_file1 = open("employees1.txt", "w")
+
+employee_file.write("Toby - HR")
+
+
+
+
+employee_file1.close()
